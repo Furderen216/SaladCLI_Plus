b850e148bcda3a23322cec24e75fbd79e74dd704
HaveFUNRICH <60838496+HaveFUNRICH@users.noreply.github.com>
2021-07-06 14:44:15 +0700

    Add Antidie Removal

---
+++ Salad CLI+ Mining only edition/REMOVE_ANTIDIE.bat
@@ -0,0 +1,3 @@
+
+reg delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers /v TdrDelay
+reg delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers /v TdrDdiDelay
