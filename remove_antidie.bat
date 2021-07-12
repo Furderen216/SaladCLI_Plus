bc00d3ced548ec81a2d776779964d80ff66fff21
WΛLKX <71191962+WalkxCode@users.noreply.github.com>
2021-07-12 10:12:04 +0200

    Create remove_antidie.bat

---
+++ remove_antidie.bat
@@ -0,0 +1,2 @@
+reg delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers /v TdrDelay
+reg delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers /v TdrDdiDelay
