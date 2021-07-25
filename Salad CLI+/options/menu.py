780197eed81279aba0098b1cd284dcddd90d13b2
HaveFUNRICH <60838496+HaveFUNRICH@users.noreply.github.com>
2021-07-25 22:46:07 +0700

    Update menu.py
    # CLI+
    #### change from 'minutes to level up' to 'minutes left until level up'
    #### add seperator \n

--- Salad CLI+/options/menu.py
+++ Salad CLI+/options/menu.py
@@ -12,17 +12,17 @@ window = win32gui.GetForegroundWindow()
 def menu(saladuser, loginscreen, logo, cookie, headers, file_handler):
 
     print(Fore.LIGHTBLACK_EX + loginscreen)
-    print(f'{Fore.RED}[----------- Profile -----------]')
+    print(f'{Fore.RED}[----------- Profile -----------]\n')
     print(f'{Fore.CYAN}Username {Fore.BLUE}> {Fore.RED}' +
           str(saladuser['username']))
     print(f'{Fore.CYAN}Email {Fore.BLUE}> {Fore.RED}' +
           str(saladuser['email']))
-    print(f'{Fore.RED}[----------- Session -----------]')
+    print(f'{Fore.RED}[----------- Session -----------]\n')
     print(f'{Fore.CYAN}Current Session {Fore.BLUE}> {Fore.RED}' +
           str(session['name']))
     print(f'{Fore.CYAN}Current level {Fore.BLUE}> {Fore.RED}' +
           str(session['currentLevelId']))
-    print(f'{Fore.CYAN}You need {Fore.BLUE}> ' + Fore.RED + str(session['totalXp'] - session['levelXp']) + Fore.BLUE +' < ' + Fore.CYAN + 'minutes to level up')
+    print(f'{Fore.CYAN}You need {Fore.BLUE}> ' + Fore.RED + str(session['totalXp'] - session['levelXp']) + Fore.BLUE +' < ' + Fore.CYAN + 'minutes left until level up')
     print("\n\n")
 
     # Input Selection
